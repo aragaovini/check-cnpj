994564a1d877211742d5dfd681633c7e04850d63
Vinicius <disanti2010@gmail.com>
2018-10-08 19:48:44 -0300

    feat: Adicionando componente de botão

---
+++ src/components/atoms/Button.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  handleClick = () => {
+    const { clickMethod } = this.props;
+    if (clickMethod && typeof clickMethod === "function") {
+      clickMethod();
+    }
+  };
+
+  render() {
+    const { text } = this.props;
+    return <button onClick={() => this.handleClick}>{text}</button>;
+  }
+}
